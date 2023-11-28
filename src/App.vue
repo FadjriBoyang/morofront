f759e8e8119f97eab12901006280589bc024fc9d
FadjriBoyang <fajriadha@gmail.com>
2023-11-28 14:56:43 +0700

    add buildspec

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="menu">
+    <Navbar />
+  </div>
+
+  <div class="main">
+    <RouterView />
+  </div>
+
+  <div class="kaki">
+    <footerComponent />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import footerComponent from './components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    footerComponent
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
+
+<style scoped>
+.menu {
+  background-color: #003366;
+  line-height: 2;
+  padding: 10px;
+}
+
+.kaki {
+  margin-bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #003366;
+  line-height: 2;
+  padding: 10px;
+}
+
+</style>
---
+++ src/views/pemerintah.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Pemerintah</h1>
+</template>
