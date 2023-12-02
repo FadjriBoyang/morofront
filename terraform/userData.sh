948c983ce44f875653978967fdd6e4931effb912
FadjriBoyang <117616974+FadjriBoyang@users.noreply.github.com>
2023-12-03 01:04:05 +0700

    Update userData.sh

--- terraform/userData.sh
+++ terraform/userData.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eo pipefail
+#! /bin/sh
 
 yum update -y
 
@@ -11,16 +11,11 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-# Configure AWS CLI with the correct region
-aws configure set aws_access_key_id <YOUR_ACCESS_KEY>
-aws configure set aws_secret_access_key <YOUR_SECRET_KEY>
-aws configure set region <AWS_REGION>
 
-# Authenticate Docker with ECR
-aws ecr get-login-password | docker login --username AWS --password-stdin <AWS_ECR_ACCOUNT_URL>
 
-# Pull the Docker image
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin <AWS_ECR_ACCOUNT_URL>
+
 docker pull <AWS_ECR_ACCOUNT_URL>/<AWS_RESOURCE_NAME_PREFIX>:latest
 
-# Run the Docker container
+
 docker run -p 8080:8080 -d <AWS_ECR_ACCOUNT_URL>/<AWS_RESOURCE_NAME_PREFIX>
