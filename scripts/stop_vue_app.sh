bbaf6b3f97ef86afd4ba12ec4ebb0d66c64b8346
FadjriBoyang <117616974+FadjriBoyang@users.noreply.github.com>
2023-12-02 17:39:30 +0700

    Update stop_vue_app.sh

--- scripts/stop_vue_app.sh
+++ scripts/stop_vue_app.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-killall node
-echo anjing kau memek
+# Identify and kill the Node.js process
+node_process_id=$(pgrep -f "node .*your-app-entry-file.js")
+if [ -n "$node_process_id" ]; then
+  kill -TERM "$node_process_id"
+  echo "Node.js process (PID $node_process_id) stopped successfully."
+else
+  echo "No running Node.js process found."
+fi
