5942ff6c2c2ac255467fd838f3689c74f21ff80a
FadjriBoyang <117616974+FadjriBoyang@users.noreply.github.com>
2023-12-03 13:42:41 +0700

    Update userData.sh

--- terraform/userData.sh
+++ terraform/userData.sh
@@ -13,7 +13,8 @@ sudo ./aws/install
 
 
 
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin <AWS_ECR_ACCOUNT_URL>
+
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY_URL
 
 docker pull <AWS_ECR_ACCOUNT_URL>/<AWS_RESOURCE_NAME_PREFIX>:latest
 
